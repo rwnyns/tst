66b5331370a81b9b311ffaa80b9d00e7abac2023
rwnyns <143508701+rwnyns@users.noreply.github.com>
2024-02-06 13:57:59 +0300

    add.php

--- add.php
+++ add.php
@@ -1,8 +1,11 @@
 <?php
-    $servername = "cosmetics-server.mysql.database.azure.com";
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "cosmetics-server.mysql.database.azure.com", "kptijfuxon", "073M2PS7XS7KD2X7$", "cosmetics-database", 3306, MYSQLI_CLIENT_SSL);
+/* $servername = "cosmetics-server.mysql.database.azure.com";
     $username = "kptijfuxon";
     $password = "FKM47YY44RU50X23$";
-    $dbname = "073M2PS7XS7KD2X7$";
+    $dbname = "073M2PS7XS7KD2X7$";*/
     
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
